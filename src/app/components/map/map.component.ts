20301c7f06ae2793c8ceefcbcd085c7eebaf43de
cesarvargasL <cesarluisvargaslezano@gmail.com>
2024-09-26 21:31:07 -0400

    feat: Update map component for use new images

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -13,6 +13,8 @@ export class MapComponent implements OnInit, OnChanges {
   @Input() public lng: number | null = null;
 
   ngOnInit(): void {
+    L.Icon.Default.imagePath = 'assets/';
+
     this.map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
