e90a6cc58dd90c85ecee9fc5ed9d5a6dea77c3b7
cesarvargasL <cesarluisvargaslezano@gmail.com>
2024-09-26 21:07:07 -0400

    feat: Update configurations and styles

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, Input, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
@@ -6,29 +6,32 @@ import * as L from 'leaflet';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnChanges {
+export class MapComponent implements OnInit, OnChanges {
   private map: L.Map | null = null;
 
-  @Input() lat: number | null = null;
-  @Input() lng: number | null = null;
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.lat && changes.lng) {
-      this.updateMap(this.lat!, this.lng!);
-    }
-  }
-
-  private updateMap(lat: number, lng: number): void {
-    if (this.map) {
-      this.map.setView([lat, lng], 13);
-      L.marker([lat, lng]).addTo(this.map).bindPopup('¡Está aquí!').openPopup();
-    }
-  }
+  @Input() public lat: number | null = null;
+  @Input() public lng: number | null = null;
 
   ngOnInit(): void {
     this.map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.lat && changes.lng && this.lat !== null && this.lng !== null) {
+      this.updateMap(this.lat, this.lng);
+    }
+  }
+
+  private updateMap(lat: number, lng: number): void {
+    if (this.map) {
+      this.map.setView([lat, lng], 13);
+      L.marker([lat, lng])
+        .addTo(this.map)
+        .bindPopup('is located here!')
+        .openPopup();
+    }
   }
 }
